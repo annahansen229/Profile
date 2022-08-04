285d21f49271f66ace00d6036c6979df7126fe94
Anna Hansen <ahansen@pdx.edu>
2022-08-03 17:41:03 -0700

    finished previous work

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,8 +1,42 @@
+// {
+//     title: "",
+//     company: "",
+//     start: "",
+//     end: "",
+//     description: "",
+//   },
+
 export const jobs = [
   {
-    title: "",
+    title: "Full Stack Software Engineer",
+    company: "Dark Matter Software",
     start: "",
-    end: "",
-    description: "",
+    end: "Current",
+    description:
+      "I maintain and develop front- and back-end features of web-based software solutions with a focus on Django and React frameworks. I work directly with clients to gather specifications, provide professional guidance towards best practices, and successfully implement new or updated features.",
+  },
+  {
+    title: "Computer Audit Specialist",
+    company: "State of Washington, Department of Revenue",
+    start: "2017",
+    end: "2021",
+    description:
+      "I was responsible for the development and maintenance of internal Audit 2000 software, written in Visual Basic for Applications, including delivering two weeks of training to all new staff. I was also responsible for internal performance reporting, which included development and maintenance of related internal databases and software, primarily in Microsoft Access. \n I represented the Audit Division in DOR's legacy systems replacement project. I was responsible for the full life cycle of a feature request, beginning with gathering requirements from Audit Division stakeholders, submitting a written technical request, advocating for the request in inter-divisional committees, working with internal and external developers to implement a solution, designing and carrying out testing, and finally providing training to staff and assessing the success of each implementation",
+  },
+  {
+    title: "Field Audit Manager",
+    company: "State of Washington, Department of Revenue",
+    start: "2015",
+    end: "2017",
+    description:
+      "I led a team of twelve professional field auditors and was responsible for a territory covering much of Southwest Washington and all of Oregon. This included hiring, development, identification and assignment of field audits, and contributing to the ongoing development of Division policies and procedures as a member of the Audit Management Team.",
+  },
+  {
+    title: "Field Auditor",
+    company: "State of Washington, Department of Revenue",
+    start: "2009",
+    end: "2015",
+    description:
+      "I independently conducted excise tax audits of varying size and complexity. As a journeyman auditor, I was also responsible for the training and development of new auditors during their first eighteen months.",
   },
 ]
--- src/routes/PreviousWork.js
+++ src/routes/PreviousWork.js
@@ -1,17 +1,31 @@
 import React from "react"
+import { jobs } from "../data/jobs"
 
 const PreviousWork = () => {
+  // create a card for each job in jobs
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">Job Title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            Begin date - End date
-          </h6>
-          <div className="card-text">description of the job</div>
-        </div>
-      </div>
+    <div className="previous-work container">
+      {jobs.map((job) => {
+        return (
+          <div className="card my-2">
+            <h5 className="card-header">{job.title}</h5>
+            <div className="card-body">
+              <h6 className="card-subtitle mb-2 text-muted">
+                {/* has start date and end date: show as range */}
+                {job.start && job.end && `${job.start} - ${job.end}`}
+                {/* missing start or end date: show which one exists (or show none) */}
+                {!(job.start && job.end) && (job.start || job.end)}
+              </h6>
+              <div className="card-text">
+                {/* if there are line breaks, this creates a p element for each paragraph */}
+                {job.description.split("\n").map((str) => (
+                  <p>{str}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
