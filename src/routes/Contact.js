c740dea6245e3e59170784bd7f8d2a3caea40389
Anna Hansen <ahansen@pdx.edu>
2022-08-08 18:12:05 -0700

    Added contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,7 +1,61 @@
 import React from "react"
+import { Row, Col, Button } from "react-bootstrap"
 
 const Contact = () => {
-  return <div className="container">Contact Section</div>
+  return (
+    <form action="">
+      <Row className="form-group">
+        <label htmlFor="name" className="col-2 col-form-label">
+          Name:
+        </label>
+        <Col>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            placeholder="Name"
+          />
+        </Col>
+      </Row>
+      <Row className="form-group">
+        <label htmlFor="email" className="col-2 col-form-label">
+          Email:
+        </label>
+        <Col>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            className="form-control"
+          />
+        </Col>
+      </Row>
+      <Row className="form-group">
+        <label htmlFor="message" className="col-2 col-form-label">
+          Message:
+        </label>
+        <Col>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            className="form-control"
+          />
+        </Col>
+      </Row>
+
+      <Row className="form-group">
+        <Col>
+          <Button type="submit" color="primary" className="btn-block">
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    </form>
+  )
 }
 
 export default Contact
