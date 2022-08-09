00f5e817dc8455f5bae0c278fe5e8fecdd0b9e9d
Anna Hansen <ahansen@pdx.edu>
2022-08-08 19:01:13 -0700

    added active styling to navbar (not working)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,39 @@
 import Nav from "react-bootstrap/Nav"
+import { useState } from "react"
 
 export default function Navbar() {
+  const [key, setKey] = useState("experience")
+
   return (
-    <Nav className="justify-content-center d-flex flex-nowrap" fill>
+    <Nav
+      fill
+      variant="tabs"
+      activeKey={key}
+      onSelect={(k) => {
+        console.log(k)
+        setKey(k)
+      }}
+      className="justify-content-center d-flex flex-nowrap mb-2"
+    >
       <Nav.Item>
-        <Nav.Link href="/">About</Nav.Link>
+        <Nav.Link href="/" eventKey="about">
+          About
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/experience">Experience</Nav.Link>
+        <Nav.Link href="/experience" eventKey="experience">
+          Experience
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/projects">Projects</Nav.Link>
+        <Nav.Link href="/projects" eventKey="projects">
+          Projects
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/contact">Contact</Nav.Link>
+        <Nav.Link href="/contact" eventKey="contact">
+          Contact
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   )
